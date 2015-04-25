73f4205957f62bc87e7c096f84c45d62ccf776c7
Tsveta Brakalova <cookie.bra@gmail.com>
2015-04-25 19:19:34 +0300

    module 7-8 exercises completed.

--- Module_7_8_Exercise/CookBook/scripts/recipeCategory.ts
+++ Module_7_8_Exercise/CookBook/scripts/recipeCategory.ts
@@ -1,8 +1,13 @@
-﻿//TODO:
-//Fill out the body of this class so it extends BaseRecipeCategory
-//and implements IRecipeCategory
-//HINT: refer to the work you did on this in Module 6 to get started 
+﻿class RecipeCategory extends BaseRecipeCategory implements IRecipeCategory {
+    description: string;
+    examples: IExample[];
 
-class RecipeCategory  {
+    constructor(recipeCategory: IRecipeCategory) {
 
+        super(recipeCategory.name, recipeCategory.foodGroups);
+        this.description = recipeCategory.description;
+        this.examples = recipeCategory.examples;
+    }
+
+    
 } 
